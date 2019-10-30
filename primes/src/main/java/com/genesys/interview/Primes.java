a6419d6b17b267dbd4a4c788117c9c50ebc236a8
Lew Gordon <lew.gordon@genesys.com>
2019-10-30 12:41:24 -0400

    add maven support

--- primes/src/main/java/com/genesys/interview/Primes.java
+++ primes/src/main/java/com/genesys/interview/Primes.java
@@ -5,25 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Primes {
-  private static List<Integer> primeList = findPrimes();
-
   public static List<Integer> findPrimes() {
-    List<Integer> integerList = Arrays.asList(2);
-
-    for (int i = 3; i < 100; i++) {
-      for (int j = 0; j <= integerList.size(); j++) {
-        if (i % j == 0) {
-          break;
-        } else if (j == integerList.size()) {
-          integerList.add(i);
-        }
-      }
-    }
-
-    return integerList;
+    return null;
   }
 
-  public static boolean isPrime(int number) {
-    return Primes.primeList.contains(number);
-  }
 }
